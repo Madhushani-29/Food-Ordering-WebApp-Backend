034ef7a2481e61a4a0854ede7c7ff7e3ead29880
madhushani-29 <madhushani2904@gmail.com>
2024-04-11 01:53:27 +0530

    database connection created

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/dbConnection";
 
 //create app
 const app=express();
+
+const port=process.env.PORT||3001;
+
+connectDB();
+
 app.use(express.json());
 app.use(cors());
 
@@ -11,7 +17,6 @@ app.get('/test', (req:Request, res:Response)=>{
     res.json({message:"Endpoint accessed successfully !"});
 });
 
-const port=process.env.PORT;
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
 })
