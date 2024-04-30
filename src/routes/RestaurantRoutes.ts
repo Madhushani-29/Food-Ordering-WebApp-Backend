7b9528b47f2e25983ce0cbfff3c60f848a2e3220
madhushani-29 <madhushani2904@gmail.com>
2024-04-30 16:18:40 +0530

    restaurant model created

--- src/routes/RestaurantRoutes.ts
+++ src/routes/RestaurantRoutes.ts
@@ -3,9 +3,29 @@ import RestaurantController from "../controller/RestaurantController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyUserRequest } from "../middleware/validation";
 import { Request, Response } from "express";
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("/", RestaurantController.createRestaurant);
+//creates a storage engine that stores uploaded files in memory (RAM) as Buffer objects
+//the uploaded files are not stored on the disk but remain in memory
+//can be useful for handling small files or cases where you don't want to save files to disk.
+const storage = multer.memoryStorage();
+const upload = multer({
+  //specifies the storage engine to be used for handling file storage
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
+
+router.post(
+  "/",
+  /*check the body for 'imageFile property and validate it with above also and 
+  if there are any validations, it will send a error message to FE, 
+  then it will append a image object can use in controller function' */
+  upload.single("imageFile"),
+  RestaurantController.createRestaurant
+);
 
 export default router;
